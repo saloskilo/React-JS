9284ba40f8fa18ebb48d125f4cce4184b7494c42
Salman Ali <salmanafridi945@gmail.com>
2024-08-13 22:59:33 +0500

    created basic structure of the app with navbar component in which i used props and pass data to app component, with bootstrap

---
+++ Text-Utils/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar title={'Logo'} />
+     
+    </>
+  );
+}
+
+export default App;
