cafd96cbd05c897a431a66f9defb1828e6c7333f
Salman Ali <143355317+saloskilo@users.noreply.github.com>
2024-08-16 08:10:26 +0500

    Added react-router-dom external lib to make my project fast and make it a single page web app.

--- Text-Utils/src/App.jsx
+++ Text-Utils/src/App.jsx
@@ -3,43 +3,61 @@ import About from "./Components/About";
 import Navbar from "./Components/Navbar";
 import Textarea from "./Components/Textarea";
 import React, { useState } from 'react'
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [mode, setmode] = useState('light');
   const [modeBtn, setmodeBtn] = useState('Enable Dark Mode')
 
-let toggleMode=()=>{
-  if (mode==='light') {
-    setmode('dark');
-    document.body.style.backgroundColor="gray";
-    setmodeBtn("Enable Light Mode");
-    document.title=("Text-Utils-Saloskilo DarkMode Enabled");
-  }else{
-    setmode('light');
-    document.body.style.backgroundColor="white";
-    setmodeBtn("Enable Dark Mode")
-    document.title=("Text-Utils-Saloskilo LightMode Enabled");
+  let toggleMode = () => {
+    if (mode === 'light') {
+      setmode('dark');
+      document.body.style.backgroundColor = "gray";
+      setmodeBtn("Enable Light Mode");
+      document.title = ("Text-Utils-Saloskilo DarkMode Enabled");
+    } else {
+      setmode('light');
+      document.body.style.backgroundColor = "white";
+      setmodeBtn("Enable Dark Mode")
+      document.title = ("Text-Utils-Saloskilo LightMode Enabled");
 
+    }
   }
-}
-  
-let toggleRedMode=()=>{
-  if (mode==='light'|| mode==='dark') {
-    // setmode('red');
-    document.body.style.backgroundColor="red";
-  }else{
-    setmode('light');
-    document.body.style.backgroundColor="white";
 
+  let toggleRedMode = () => {
+    if (mode === 'light' || mode === 'dark') {
+      // setmode('red');
+      document.body.style.backgroundColor = "red";
+    } else {
+      setmode('light');
+      document.body.style.backgroundColor = "white";
+
+    }
   }
-}
   return (
     <>
-      <Navbar title={'SaloSkilo'} mode={mode} toggleMode={toggleMode} toggleRedMode={toggleRedMode} modeBtn={modeBtn}/>
-      <Textarea heading={'Text-Area'}/>
-      {/* <About/> */}
-     
+      <Router>
+        <Navbar title={'SaloSkilo'} mode={mode} toggleMode={toggleMode} toggleRedMode={toggleRedMode} modeBtn={modeBtn} />
+        
+
+          <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/">
+              <Textarea heading={'Text-Area'} />
+            </Route>
+          </Switch>
+
+          
+
+       
+      </Router>
     </>
   );
 }
--- Text-Utils/src/Components/Navbar.jsx
+++ Text-Utils/src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from "react-router-dom";
 
 
 const Navbar = (props) => {
@@ -8,9 +9,9 @@ const Navbar = (props) => {
     <><div className="container">
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <Link className="navbar-brand" to="/">
             {props.title}
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -25,23 +26,23 @@ const Navbar = (props) => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <Link className="nav-link active" aria-current="page" to="/">
                   Home
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link " aria-current="page" href="#">
+                <Link className="nav-link " aria-current="page" to="/about">
                   About
-                </a>
+                </Link>
               </li>
             </ul>
 
           </div>
-          <div className={`mt-2 form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+          <div className={`mt-2 form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
             <input className="form-check-input " onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
             <label className="form-check-label " htmlFor="flexSwitchCheckDefault">{props.modeBtn}</label>
           </div>
-          <div className={` mt-2 form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+          <div className={` mt-2 form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
             <input className="form-check-input " onClick={props.toggleRedMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
             <label className="form-check-label " htmlFor="flexSwitchCheckDefault">Red Mode</label>
           </div>
