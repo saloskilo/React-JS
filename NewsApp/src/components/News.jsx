6e7a54ed24578467734480115b52aed5e32e37a8
Salman Ali <143355317+saloskilo@users.noreply.github.com>
2024-08-18 03:01:01 +0500

    added something

--- NewsApp/src/components/News.jsx
+++ NewsApp/src/components/News.jsx
@@ -4,24 +4,25 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-        <div className='container'>
-      <h2>SaloSkilo Top-News</h2>
-      <div className="row">
-        <div className="col md-4">
-        <NewsItem title={"myTitle"} description={"myDesc"}/>
+      <div className='container'>
+        <h2>SaloSkilo Top-News</h2>
+
+        <div className="row">
+          <div className="col md-4">
+            <NewsItem title={"myTitle"} description={"myDesc"} imageUrl={'https://images.macrumors.com/t/8HvbMqisozjrhE8YM0x45tE4DDI=/400x400/smart/article-new/2024/08/sonny-iphone-16-pro-colors.jpg'} />
+
+          </div>
+          <div className="col md-4">
+            <NewsItem title={"myTitle"} description={"myDesc"} />
+
+          </div>
+          <div className="col md-4">
+            <NewsItem title={"myTitle"} description={"myDesc"} />
+
+          </div>
 
         </div>
-        <div className="col md-4">
-        <NewsItem title={"myTitle"} description={"myDesc"}/>
 
-        </div>
-        <div className="col md-4">
-        <NewsItem title={"myTitle"} description={"myDesc"}/>
-
-        </div>
-
-      </div>
-      
 
       </div>
     )
--- NewsApp/src/components/NewsItem.jsx
+++ NewsApp/src/components/NewsItem.jsx
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let {title , description }=this.props;
+    let {title , description,imageUrl }=this.props;
 
     return (
         <div className="card" style={{width: "18rem"}}>
-          <img src="https://images.macrumors.com/t/8HvbMqisozjrhE8YM0x45tE4DDI=/400x400/smart/article-new/2024/08/sonny-iphone-16-pro-colors.jpg" className="card-img-top" alt="" />
+          <img src={imageUrl} className="card-img-top" alt="" />
           <div className="card-body">
             <h5 className="card-title"> {title}</h5>
             <p className="card-text">
