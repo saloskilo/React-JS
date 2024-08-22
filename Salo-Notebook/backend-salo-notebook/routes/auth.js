bc42e3ba92d209112b8c4774d52945dc3a306009
Salman Ali <143355317+saloskilo@users.noreply.github.com>
2024-08-22 11:27:24 +0500

    making collection in vs code for thunder. and added different validation in the auth,js and make it in the try catch. creating new user a add the data in database

--- Salo-Notebook/backend-salo-notebook/routes/auth.js
+++ Salo-Notebook/backend-salo-notebook/routes/auth.js
@@ -3,25 +3,40 @@ const Router = express.Router();
 const User = require('../models/Users');
 const { body, validationResult } = require('express-validator');
 
+// create user using post method "/api/auth/createuser". No login required
+Router.post('/createuser', [
+  body('name', "Enter Valid Name").isLength({ min: 3 }),
+  body('email', "Type Valid Email").isEmail(),
+  body('password', "Password should be length 5").isLength({ min: 5 })
 
-Router.post('/',[
-    body('name',"Enter Valid Name").isLength({min:3}),
-    body('email',"Type Valid Email").isEmail(),
-    body('password',"Password should be length 5").isLength({min:5})
+], async (req, res) => {
+  console.log(req.body);
+  // if there are error, send bad request and errors 
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    
+ 
+  // check wheather the user exist 
+  let user = await User.findOne({ email: req.body.email });
+  console.log(user);
+  
+  if (user) {
+    return res.status(400).json({ error: "User with the email exist" })
+  }
+  user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
 
-], (req, res) => {
-    console.log(req.body);
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({errors:errors.array()});
-    }
-  User.create({
-    name:req.body.name,
-    email:req.body.email,
-    password:req.body.password
-
-  }).then(user=>{res.json(user)})
-    // res.send(req.body)
-
+  })
+  // .then(user=>{res.json(user)})
+  res.json({user})
+} catch (error) {
+    console.error(error.message);
+    res.status(500).send("Something went wrong")
+}
 });
 module.exports = Router;
