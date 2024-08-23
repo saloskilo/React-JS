f86b801cf72dfb40dc7e83815206a14078abfafc
Salman Ali <143355317+saloskilo@users.noreply.github.com>
2024-08-23 06:54:32 +0500

    added two dependencies. bcrypt and jwt token in auth.js for making password into a hash value to store it in db for security. jwt for giving a special token to every single user for authentication.

--- Salo-Notebook/backend-salo-notebook/routes/auth.js
+++ Salo-Notebook/backend-salo-notebook/routes/auth.js
@@ -2,6 +2,9 @@ const express = require('express');
 const Router = express.Router();
 const User = require('../models/Users');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'salmanalisaloskil0';
 
 // create user using post method "/api/auth/createuser". No login required
 Router.post('/createuser', [
@@ -17,26 +20,35 @@ Router.post('/createuser', [
     return res.status(400).json({ errors: errors.array() });
   }
   try {
-    
- 
-  // check wheather the user exist 
-  let user = await User.findOne({ email: req.body.email });
-  console.log(user);
-  
-  if (user) {
-    return res.status(400).json({ error: "User with the email exist" })
-  }
-  user = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
 
-  })
-  // .then(user=>{res.json(user)})
-  res.json({user})
-} catch (error) {
+
+    // check wheather the user exist 
+    let user = await User.findOne({ email: req.body.email });
+    console.log(user);
+
+    if (user) {
+      return res.status(400).json({ error: "User with the email exist" })
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt)
+    // create new user 
+    user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: secPass
+
+    })
+    const data = {
+      user: { id: user.id }
+    }
+    const authToken = jwt.sign(data, JWT_SECRET);
+    console.log(authToken);
+
+    res.json({ user })
+  } catch (error) {
     console.error(error.message);
     res.status(500).send("Something went wrong")
-}
+  }
 });
 module.exports = Router;
