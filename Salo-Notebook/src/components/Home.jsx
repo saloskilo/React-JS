821b32382b72f81ad5b085b7abee08563535c79c
Salman Ali <143355317+saloskilo@users.noreply.github.com>
2024-09-01 04:50:45 +0500

    Fetching Notes from Context

--- Salo-Notebook/src/components/Home.jsx
+++ Salo-Notebook/src/components/Home.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import noteContext from '../context/notes/NoteContext';
 
 const Home = () => {
+
+  const context = useContext(noteContext);
+  const { notes, setNotes } = context;
+  console.log(notes);
+
   return (
     <div>
       <div className='container my-3'>
@@ -25,6 +31,11 @@ const Home = () => {
         <div className="container my-3">
           <h2>Your Notes</h2>
         </div>
+
+        {notes.map((notes) => {
+          return notes.title;
+        })}
+        
       </div>
     </div>
   )
--- Salo-Notebook/src/context/notes/NoteState.jsx
+++ Salo-Notebook/src/context/notes/NoteState.jsx
@@ -16,7 +16,31 @@ const NoteState = (props) => {
     //     }, 1000);
     // }
 
-    return (<noteContext.Provider value={{}}>
+
+    const noteInitial=[
+    
+        {
+          "_id": "66cfda6cdda9d93bf2dad711",
+          "user": "66c7eb1110400d7c51d51039",
+          "title": "my Title 1",
+          "description": "this is description",
+          "tag": "title",
+          "date": "2024-08-29T02:18:20.812Z",
+          "__v": 0
+        },
+        {
+          "_id": "66cfda6ddda9d93bf2dad713",
+          "user": "66c7eb1110400d7c51d51039",
+          "title": "my Title 2",
+          "description": "this is description",
+          "tag": "title",
+          "date": "2024-08-29T02:18:21.115Z",
+          "__v": 0
+        }
+      ]
+
+      const [notes, setnotes] = useState(noteInitial)
+    return (<noteContext.Provider value={{notes,setnotes}}>
         {props.children}
     </noteContext.Provider>
     )
