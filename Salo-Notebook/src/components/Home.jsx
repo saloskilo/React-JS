9d50d009ca34c22b94a9e724f790edf202539fb2
Salman Ali <143355317+saloskilo@users.noreply.github.com>
2024-09-02 07:11:59 +0500

    added Addnote Component with added functionality and can easily add a new note. make changes in the foorm as well.

--- Salo-Notebook/src/components/Home.jsx
+++ Salo-Notebook/src/components/Home.jsx
@@ -5,25 +5,8 @@ const Home = () => {
 
   return (
     <div>
-      <div className='container my-3'>
-        <h2>Add Notes</h2>
-        <form className='my-3'>
-          <div className="mb-3">
-            <label htmlFor="titleInput" className="form-label">Title</label>
-            <input type="text" className="form-control" id="titleInput" aria-describedby="titleHelp" />
-            <div id="titleHelp" className="form-text">Enter the title for your note.</div>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="descriptionInput" className="form-label">Description</label>
-            <textarea className="form-control" id="descriptionInput" rows="3"></textarea>
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-
-
-
-        <Notes />
-      </div>
+     
+      <Notes />
     </div>
   )
 }
--- Salo-Notebook/src/context/notes/NoteState.jsx
+++ Salo-Notebook/src/context/notes/NoteState.jsx
@@ -4,98 +4,122 @@ import noteContext from './NoteContext'
 
 const NoteState = (props) => {
 
-    // const s1 = {
-    //     "name": "salman",
-    // }
-    // const [state, setstate] = useState(s1);
-    // const update = () => {
-    //     setTimeout(() => {
-    //         setstate({
-    //             "name": "Ali",
-    //         })
-    //     }, 1000);
-    // }
+  // const s1 = {
+  //     "name": "salman",
+  // }
+  // const [state, setstate] = useState(s1);
+  // const update = () => {
+  //     setTimeout(() => {
+  //         setstate({
+  //             "name": "Ali",
+  //         })
+  //     }, 1000);
+  // }
 
 
-    const noteInitial=[
-    
-        {
-          "_id": "66cfda62cdda9d93bf2dad711",
-          "user": "66c7eb1110400d7c51d51039",
-          "title": "my Title 1",
-          "description": "this is description",
-          "tag": "title",
-          "date": "2024-08-29T02:18:20.812Z",
-          "__v": 0
-        },
-        {
-          "_id": "66cfda6ddda39d93bf2dad713",
-          "user": "66c7eb1110400d7c51d51039",
-          "title": "my Title 2",
-          "description": "this is description",
-          "tag": "title",
-          "date": "2024-08-29T02:18:21.115Z",
-          "__v": 0
-        },
-        {
-          "_id": "66cfda6cdda9d93bf2da5d711",
-          "user": "66c7eb1110400d7c51d51039",
-          "title": "my Title 1",
-          "description": "this is description",
-          "tag": "title",
-          "date": "2024-08-29T02:18:20.812Z",
-          "__v": 0
-        },
-        {
-          "_id": "66cfda6ddda9d93bf2dad713",
-          "user": "66c37eb1110400d7c51d51039",
-          "title": "my Title 2",
-          "description": "this is description",
-          "tag": "title",
-          "date": "2024-08-29T02:18:21.115Z",
-          "__v": 0
-        }, {
-          "_id": "66cfda6cdda9d93bf25dad711",
-          "user": "66c7eb1110400d7c51d51039",
-          "title": "my Title 1",
-          "description": "this is description",
-          "tag": "title",
-          "date": "2024-08-29T02:18:20.812Z",
-          "__v": 0
-        },
-        {
-          "_id": "66cfda6ddda9d934bf2dad713",
-          "user": "66c7eb1110400d7c51d51039",
-          "title": "my Title 2",
-          "description": "this is description",
-          "tag": "title",
-          "date": "2024-08-29T02:18:21.115Z",
-          "__v": 0
-        }, {
-          "_id": "66cfd2a6cdda9d93bf2dad711",
-          "user": "66c7eb1110400d7c51d51039",
-          "title": "my Title 1",
-          "description": "this is description",
-          "tag": "title",
-          "date": "2024-08-29T02:18:20.812Z",
-          "__v": 0
-        },
-        {
-          "_id": "66cfda6ddda93d93bf2dad713",
-          "user": "66c7eb1110400d7c51d51039",
-          "title": "my Title 2",
-          "description": "this is description",
-          "tag": "title",
-          "date": "2024-08-29T02:18:21.115Z",
-          "__v": 0
-        }
-      ]
+  const noteInitial = [
 
-      const [notes, setnotes] = useState(noteInitial)
-    return (<noteContext.Provider value={{notes,setnotes}}>
-        {props.children}
-    </noteContext.Provider>
-    )
+    {
+      "_id": "66cfda62cdda9d93bf2dad711",
+      "user": "66c7eb1110400d7c51d51039",
+      "title": "my Title 1",
+      "description": "this is description",
+      "tag": "title",
+      "date": "2024-08-29T02:18:20.812Z",
+      "__v": 0
+    },
+    {
+      "_id": "66cfda6ddda39d93bf2dad713",
+      "user": "66c7eb1110400d7c51d51039",
+      "title": "my Title 2",
+      "description": "this is description",
+      "tag": "title",
+      "date": "2024-08-29T02:18:21.115Z",
+      "__v": 0
+    },
+    {
+      "_id": "66cfda6cdda9d93bf2da5d711",
+      "user": "66c7eb1110400d7c51d51039",
+      "title": "my Title 1",
+      "description": "this is description",
+      "tag": "title",
+      "date": "2024-08-29T02:18:20.812Z",
+      "__v": 0
+    },
+    {
+      "_id": "66cfda6ddda9d93bf2dad713",
+      "user": "66c37eb1110400d7c51d51039",
+      "title": "my Title 2",
+      "description": "this is description",
+      "tag": "title",
+      "date": "2024-08-29T02:18:21.115Z",
+      "__v": 0
+    }, {
+      "_id": "66cfda6cdda9d93bf25dad711",
+      "user": "66c7eb1110400d7c51d51039",
+      "title": "my Title 1",
+      "description": "this is description",
+      "tag": "title",
+      "date": "2024-08-29T02:18:20.812Z",
+      "__v": 0
+    },
+    {
+      "_id": "66cfda6ddda9d934bf2dad713",
+      "user": "66c7eb1110400d7c51d51039",
+      "title": "my Title 2",
+      "description": "this is description",
+      "tag": "title",
+      "date": "2024-08-29T02:18:21.115Z",
+      "__v": 0
+    }, {
+      "_id": "66cfd2a6cdda9d93bf2dad711",
+      "user": "66c7eb1110400d7c51d51039",
+      "title": "my Title 1",
+      "description": "this is description",
+      "tag": "title",
+      "date": "2024-08-29T02:18:20.812Z",
+      "__v": 0
+    },
+    {
+      "_id": "66cfda6ddda93d93bf2dad713",
+      "user": "66c7eb1110400d7c51d51039",
+      "title": "my Title 2",
+      "description": "this is description",
+      "tag": "title",
+      "date": "2024-08-29T02:18:21.115Z",
+      "__v": 0
+    }
+  ]
+  // Add Notes 
+  const addNote = (title, description, tag) => {
+    const note = {
+      "_id": "66cfda42cdda9dss93bf2dad711",
+      "user": "66c7eb1110400d7c51d51039",
+      "title": title,
+      "description": description,
+      "tag": tag, 
+      "date": "2024-08-29T02:18:20.812Z",
+      "__v": 0
+    };
+    setnotes(notes.concat(note))
+  }
+
+  // Delete Notes 
+  const deleteNote = () => { }
+
+
+
+  // Edit Notes 
+  const editNote = () => { }
+
+
+
+
+  const [notes, setnotes] = useState(noteInitial)
+  return (<noteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
+    {props.children}
+  </noteContext.Provider>
+  )
 }
 
 export default NoteState;
